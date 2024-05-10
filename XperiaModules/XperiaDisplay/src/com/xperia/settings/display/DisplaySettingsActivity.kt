e93af1e474495168fe340dd5b6c3de68193a260d
Peter Kalauskas <peskal@google.com>
2024-05-10 13:53:13 +0000

    XperiaModules: Enable use_resource_processor for all sysui deps This will make the build faster
    Test: m checkbuild
    Bug: 295208392
    Signed-off-by: RisenID <risen@pixelexperience.org>
    Change-Id: I0c1bd901429bbe3bf81c1530e156735f8637a96e

--- XperiaModules/XperiaDisplay/src/com/xperia/settings/display/DisplaySettingsActivity.kt
+++ XperiaModules/XperiaDisplay/src/com/xperia/settings/display/DisplaySettingsActivity.kt
@@ -9,14 +9,13 @@ package com.xperia.settings.display
 import android.os.Bundle
  
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
-import com.android.settingslib.widget.R
 
 class DisplaySettingsActivity : CollapsingToolbarBaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fragmentManager
                 .beginTransaction()
-                .replace(R.id.content_frame, DisplaySettingsFragment(), TAG)
+                .replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame, DisplaySettingsFragment(), TAG)
                 .commit()
     }
 
