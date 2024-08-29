3647f637d0cc2bfc4655a669b41abd38c6d7b323
ahnet-69 <syedmusicrealise@gmail.com>
2024-08-29 14:40:59 +0000

    XperiaDisplay: Use SwitchPreference instead of SwitchPreferenceCompat
    * SwitchPreferenceCompat doesnt exist in aospa

--- XperiaModules/XperiaDisplay/src/com/xperia/settings/display/preferences/SecureSettingSwitchPreference.kt
+++ XperiaModules/XperiaDisplay/src/com/xperia/settings/display/preferences/SecureSettingSwitchPreference.kt
@@ -21,9 +21,9 @@ import android.content.Context
 import android.provider.Settings
 import android.os.UserHandle
 import android.util.AttributeSet
-import androidx.preference.SwitchPreferenceCompat
+import androidx.preference.SwitchPreference
 
-class SecureSettingSwitchPreference : SwitchPreferenceCompat {
+class SecureSettingSwitchPreference : SwitchPreference {
 
    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
