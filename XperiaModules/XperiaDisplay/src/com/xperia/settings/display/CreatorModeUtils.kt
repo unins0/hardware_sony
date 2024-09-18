c4d3ef4cda7bde7c3818ab8d4977c9306d30c2ec
lolipuru <banstellazz@gmail.com>
2024-09-18 21:10:40 +0300

    XperiaDisplay: Tune X-Reality Pro
    Signed-off-by: saku-bruh <saku-bruh@proton.me>
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- XperiaModules/XperiaDisplay/src/com/xperia/settings/display/CreatorModeUtils.kt
+++ XperiaModules/XperiaDisplay/src/com/xperia/settings/display/CreatorModeUtils.kt
@@ -6,35 +6,102 @@
 
 package com.xperia.settings.display
 
+import android.app.ActivityTaskManager
 import android.content.Context
+import android.os.RemoteException
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import androidx.core.content.ContextCompat
+import android.hardware.display.ColorDisplayManager
+
+import vendor.semc.hardware.display.V2_0.IDisplay
+import vendor.semc.hardware.display.V2_0.IDisplayCallback
 import vendor.semc.hardware.display.V2_0.PccMatrix
 
-class CreatorModeUtils(context: Context) : DisplayEngine(context) {
-    override val modeEnableSetting: String
-        get() = CREATOR_MODE_ENABLE
+import com.xperia.settings.display.server.DisplayTransformManager
 
-    override val enabledColorMode: Int
-        get() = 0
+class CreatorModeUtils(private val context: Context) : IDisplayCallback.Stub() {
+    private val colorDisplayManager: ColorDisplayManager =
+            context.getSystemService(ColorDisplayManager::class.java)
+                    ?: throw Exception("Display manager is NULL")
+    private val semcDisplayService: IDisplay by lazy {
+        val service = IDisplay.getService() ?: throw Exception("SEMC Display HIDL not found")
+        service.setup()
+        service
+    }
 
-    override val disabledColorMode: Int
-        get() = 3
+    private val LEVEL_COLOR_MATRIX_CREATOR_MODE = 5690
 
-    override val enabledSsspColorMode: Int
-        get() = 0
+    private val dtm: DisplayTransformManager = DisplayTransformManager()
 
-    override val disabledSsspColorMode: Int
-        get() = 1
+    private val MAX = 255f
 
-    override fun getMatrixArray(matrix: PccMatrix): FloatArray {
-        return floatArrayOf(
-            matrix.red, matrix.green, matrix.blue, 0f, 0f,
-            matrix.red, matrix.green, matrix.blue, 0f, 0f,
-            matrix.red, matrix.green, matrix.blue, 0f, 0f,
-            0f, 0f, 0f, 1f, 0f
-        )
+    val isEnabled: Boolean
+        get() = Settings.Secure.getInt(context.contentResolver, CREATOR_MODE_ENABLE, 0) != 0
+
+    fun setMode(enabled: Boolean) {
+        semcDisplayService.set_sspp_color_mode(if (enabled) 0 else 1)
+        colorDisplayManager.setColorMode(if (enabled) 0 else 3)
+        semcDisplayService.set_color_mode(if (enabled) 0 else 1)
+
+        Settings.Secure.putInt(context.contentResolver, CREATOR_MODE_ENABLE, if (enabled) 1 else 0)
+    }
+
+    fun initialize() {
+        Log.i(TAG, "Creator Mode controller setup")
+        try {
+            if (!isEnabled) {
+                semcDisplayService.set_sspp_color_mode(1)
+                colorDisplayManager.setColorMode(3)
+                semcDisplayService.set_color_mode(1)
+            }
+
+            semcDisplayService.registerCallback(this)
+    
+            if (isEnabled) {
+                setMode(true)
+            }
+        } catch (e: Exception) {
+        }
+    }
+
+    override fun onWhiteBalanceMatrixChanged(matrix: PccMatrix) {
+        val r = matrix.red
+        val g = matrix.green
+        val b = matrix.blue
+
+        try {
+            dtm.setColorMatrix(LEVEL_COLOR_MATRIX_CREATOR_MODE,
+                floatArrayOf(
+                        r,  0f, 0f, 0f,
+                        0f, g,  0f, 0f,
+                        0f, 0f, b,  0f,
+                        0f, 0f, 0f, 1f
+                    ))
+        } catch (e: Exception) {
+            Log.e(TAG, "Could not apply setColorMatrix", e)
+            return;
+        }
+
+        updateConfiguration()
+
+        Log.i(TAG, "New white balance: ${r}, ${g}, ${b}")
+    }
+
+    fun updateConfiguration() {
+        try {
+            ActivityTaskManager.getService().updateConfiguration(null)
+        } catch (e: RemoteException) {
+            Log.e(TAG, "Could not update configuration", e)
+        }
     }
 
     companion object {
+        private const val TAG = "CreatorModeUtils"
         private const val CREATOR_MODE_ENABLE = "cm_enable"
     }
 }
