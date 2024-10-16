e4e2695264909a05eed6e2bdc9c2267bbbaf5c2b
ahnet-69 <syedmusicrealise@gmail.com>
2024-10-16 13:48:00 +0500

    hidl: fp: Force it only build egis for edo

--- hidl/fp/Android.mk
+++ hidl/fp/Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(PRODUCT_PLATFORM_SOD),true)
-ifneq ($(TARGET_DEVICE_NO_FPC), true)
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -13,75 +11,11 @@ LOCAL_SRC_FILES := \
     QSEEComFunc.c \
     common.c
 
-# ---------------- FPC ----------------
-ifeq ($(filter-out loire tone,$(SOMC_PLATFORM)),)
-LOCAL_SRC_FILES += fpc_imp_loire_tone.c
-HAS_FPC := true
-endif
-
-ifeq ($(filter-out yoshino,$(SOMC_PLATFORM)),)
-LOCAL_SRC_FILES += fpc_imp_yoshino_nile_tama.c
-HAS_FPC := true
 LOCAL_CFLAGS += \
-    -DUSE_FPC_YOSHINO
-endif
-
-ifeq ($(filter-out nile,$(SOMC_PLATFORM)),)
-# NOTE: Nile can have either FPC or Egistec
-LOCAL_SRC_FILES += fpc_imp_yoshino_nile_tama.c
-HAS_FPC := true
-LOCAL_CFLAGS += \
-    -DUSE_FPC_NILE \
-    -DHAS_LEGACY_EGISTEC
-endif
-
-ifeq ($(filter-out tama,$(SOMC_PLATFORM)),)
-LOCAL_SRC_FILES += fpc_imp_yoshino_nile_tama.c
-HAS_FPC := true
-LOCAL_CFLAGS += \
-    -DUSE_FPC_TAMA
-endif
-
-# ---------------- Egistec ----------------
-ifneq ($(filter-out loire tone yoshino tama,$(SOMC_PLATFORM)),)
-LOCAL_CFLAGS += -DFINGERPRINT_TYPE_EGISTEC
-endif
-
-# Firmware custom location
-ifeq ($(filter-out kumano seine edo sagami nagara yodo,$(SOMC_PLATFORM)),)
-LOCAL_CFLAGS += \
-    -DEGIS_QSEE_APP_PATH=\"/vendor/firmware\"
-endif
-
-# Firmware name and atributes
-ifeq ($(filter-out kumano seine edo sagami lena murray,$(SOMC_PLATFORM)),)
-LOCAL_CFLAGS += \
+    -DFINGERPRINT_TYPE_EGISTEC \
+    -DEGIS_QSEE_APP_PATH=\"/vendor/firmware\" \
     -DEGISTEC_SAVE_TEMPLATE_RETURNS_SIZE \
     -DEGIS_QSEE_APP_NAME=\"egista\"
-else ifeq ($(filter-out nagara,$(SOMC_PLATFORM)),)
-LOCAL_CFLAGS += \
-    -DEGISTEC_SAVE_TEMPLATE_RETURNS_SIZE \
-    -DEGIS_QSEE_APP_NAME=\"egista64\"
-else ifeq ($(filter-out zambezi yodo,$(SOMC_PLATFORM)),)
-LOCAL_CFLAGS += \
-    -DEGISTEC_SAVE_TEMPLATE_RETURNS_SIZE \
-    -DEGIS_QSEE_APP_NAME=\"egisap\"
-else
-LOCAL_CFLAGS += \
-    -DEGIS_QSEE_APP_NAME=\"egisap32\"
-endif
-
-# Define dynamic power management for everything but the following platforms:
-ifneq ($(filter-out kumano seine,$(SOMC_PLATFORM)),)
-LOCAL_CFLAGS += -DHAS_DYNAMIC_POWER_MANAGEMENT
-endif
-
-ifneq ($(HAS_FPC),true)
-# This file heavily depends on fpc_ implementations from the
-# above fpc_imp_* files. There is no sensible default file
-# on some platforms, so just remove the file altogether:
-LOCAL_SRC_FILES -= BiometricsFingerprint.cpp
-endif
 
 LOCAL_SHARED_LIBRARIES := \
     android.hardware.biometrics.fingerprint@2.1 \
@@ -103,5 +37,3 @@ LOCAL_CFLAGS += \
     -fexceptions
 
 include $(BUILD_EXECUTABLE)
-endif
-endif # PRODUCT_PLATFORM_SOD
