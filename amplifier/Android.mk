6131ad590433b2f739237defe01446fdfae02d98
Alcatraz323 <alcatraz32323@gmail.com>
2025-01-15 12:50:47 +0800

    amplifier: Guard with BOARD_USES_ALSA_AUDIO
    * Support prebuilt audio case
    
    Change-Id: I03a4bc7be18d7be0cce67ab9eec94246e3ec6768

--- amplifier/Android.mk
+++ amplifier/Android.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -49,3 +49,4 @@ LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
+endif
